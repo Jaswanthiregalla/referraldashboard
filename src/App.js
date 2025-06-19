a40cc12f2ab6cd3ddf3c1b8dcb385c3c253c2deb
Jaswanthiregalla <jaswanthiregalla2655@gmail.com>
2025-06-19 08:47:15 -0700

    Added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import DashboardPage from "./pages/ReferralPage"; // your referral dashboard
 import LoginPage from "./pages/LoginPage";
 function App() {
